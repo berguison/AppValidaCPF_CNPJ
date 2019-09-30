2d0873b9500deca5ff09eb1cb56415f88ce0fd0f
berguison <enribergui.tecnico@gmail.com>
2019-09-30 15:07:52 -0300

    Adicionar arquivos de projeto.

---
+++ AppValidaCPF_CNPJ/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace AppValidaCPF_CNPJ
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_validar_cpf = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.btn_validar_cnpj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 75);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CPF";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(65, 202);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Validar CNPJ";
+            // 
+            // btn_validar_cpf
+            // 
+            this.btn_validar_cpf.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_validar_cpf.Location = new System.Drawing.Point(68, 141);
+            this.btn_validar_cpf.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_validar_cpf.Name = "btn_validar_cpf";
+            this.btn_validar_cpf.Size = new System.Drawing.Size(534, 42);
+            this.btn_validar_cpf.TabIndex = 2;
+            this.btn_validar_cpf.Text = "Validar CPF";
+            this.btn_validar_cpf.UseVisualStyleBackColor = true;
+            this.btn_validar_cpf.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(68, 107);
+            this.maskedTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.maskedTextBox1.Mask = "000.000.000-00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(534, 26);
+            this.maskedTextBox1.TabIndex = 3;
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox2.Location = new System.Drawing.Point(68, 235);
+            this.maskedTextBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.maskedTextBox2.Mask = "00.000.000/0000-00";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(534, 26);
+            this.maskedTextBox2.TabIndex = 3;
+            // 
+            // btn_validar_cnpj
+            // 
+            this.btn_validar_cnpj.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_validar_cnpj.Location = new System.Drawing.Point(68, 282);
+            this.btn_validar_cnpj.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_validar_cnpj.Name = "btn_validar_cnpj";
+            this.btn_validar_cnpj.Size = new System.Drawing.Size(534, 42);
+            this.btn_validar_cnpj.TabIndex = 2;
+            this.btn_validar_cnpj.Text = "Validar CNPJ";
+            this.btn_validar_cnpj.UseVisualStyleBackColor = true;
+            this.btn_validar_cnpj.Click += new System.EventHandler(this.btn_validar_cnpj_Click);
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 452);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.btn_validar_cnpj);
+            this.Controls.Add(this.btn_validar_cpf);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "FormPrincipal";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Validador de CPF e CNPJ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_validar_cpf;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Button btn_validar_cnpj;
+    }
+}
+
---
+++ AppValidaCPF_CNPJ/Form1.cs
@@ -0,0 +1,410 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppValidaCPF_CNPJ
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cpf = maskedTextBox1.Text.Replace(",","").Replace("-","");
+            if (cpf.Length==11) {
+                ValidarCPF(cpf);
+            }
+            else
+            {
+                MessageBox.Show("Número do CPF menor que 11 números.","Informação de validação.");
+                maskedTextBox1.Clear();
+                maskedTextBox1.Focus();
+            }
+        }
+
+        private void btn_validar_cnpj_Click(object sender, EventArgs e)
+        {
+            string cnpj = maskedTextBox2.Text.Replace(",","").Replace("/","").Replace("-","");
+            if (cnpj.Length == 14)
+            {
+                ValidarCNPJ(cnpj);
+            }
+            else
+            {
+                MessageBox.Show("Número do CNPJ menor que 14 números.", "Informação de validação.");
+                maskedTextBox2.Clear();
+                maskedTextBox2.Focus();
+            }
+
+        }
+
+        private bool ValidarCPF(string nu_cpf)
+        {
+            bool icStatus = false;
+            bool ic_status1 = false;
+            bool ic_status2 = false;
+            int mult = 0;
+            int cont = 10;
+            int soma = 0;
+            int result = 0;
+            string nucpf1 = "";
+            string nu_cpf2 = "";
+            
+            try
+            {
+                nucpf1 = nu_cpf.Substring(0,9);
+                foreach (var item in nucpf1.ToList())
+                {
+
+                    mult = int.Parse(item.ToString()) * cont;
+
+                    soma += mult;
+                    cont--;
+                }
+
+                result = (soma * 10) % 11;
+                if (result ==int.Parse(nu_cpf.Substring(9,1)))
+                {
+                    ic_status1 = true;
+                }
+             
+                cont = 11;
+
+                soma = 0;
+                nu_cpf2 = nu_cpf.Substring(0, 10);
+                foreach (var item in nu_cpf2.ToList())
+                {
+
+                    mult = int.Parse(item.ToString()) * cont;
+
+                    soma += mult;
+                    cont--;
+                }
+                result = (soma * 10) % 11;
+                if (result == int.Parse(nu_cpf.Substring(10,1)))
+                {
+                    ic_status2 = true;
+                }
+
+                if (ic_status1==true && ic_status2==true)
+                {
+                    MessageBox.Show("CPF valido.", "Informação de validação.");
+                }
+                else if (ic_status1 == false && ic_status2 == false)
+                {
+                    MessageBox.Show("CPF inválido.", "Informação de validação.");
+                }
+
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Problema com a validação.", "Informação de validação.");
+
+            }
+
+
+            return icStatus;
+        }
+
+        private bool ValidarCNPJ(string nu_cnpj)
+        {
+            bool icStatus = false;
+            bool ic_status1 = false;
+            bool ic_status2 = false;
+            int mult = 0;
+            int[] multiplicadores01 = { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicadores02 = { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int posicao = 1;
+            int soma = 0;
+            int result = 0;
+            string nucpf1 = "";
+            string nu_cpf2 = "";
+
+            try
+            {
+                nucpf1 = nu_cnpj.Substring(0, 12);
+                foreach (var item in nucpf1.ToList())
+                {
+
+                    switch (posicao)
+                    {
+                        case 1:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores01[0];
+
+                            soma += mult;
+                            
+                            break;
+                        case 2:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores01[1];
+
+                            soma += mult;
+
+                            break;
+                        case 3:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores01[2];
+
+                            soma += mult;
+
+                            break;
+                        case 4:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores01[3];
+
+                            soma += mult;
+
+                            break;
+                        case 5:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores01[4];
+
+                            soma += mult;
+
+                            break;
+
+                        case 6:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores01[5];
+
+                            soma += mult;
+
+                            break;
+
+                        case 7:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores01[6];
+
+                            soma += mult;
+
+                            break;
+
+                        case 8:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores01[7];
+
+                            soma += mult;
+
+                            break;
+
+                        case 9:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores01[8];
+
+                            soma += mult;
+
+                            break;
+
+                        case 10:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores01[9];
+
+                            soma += mult;
+
+                            break;
+                        case 11:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores01[10];
+
+                            soma += mult;
+
+                            break;
+                        case 12:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores01[11];
+
+                            soma += mult;
+
+                            break;
+                       
+                        default:
+                            break;
+
+                       }
+                    posicao++;
+                }
+
+                result = soma  % 11;
+                int dv1 = int.Parse(nu_cnpj.Substring(12, 1));
+                if (result<2 )
+                {
+                    if(result == dv1)
+                    {
+                        ic_status1 = true;
+                    }
+                   
+                }
+                else if (result >=2)
+                {
+                    result = 11 - result;
+                    if (result == int.Parse(nu_cnpj.Substring(12, 1))) { ic_status1 = true; }
+                    
+                }
+
+
+                int dv2 = 0;
+                soma = 0;
+                posicao = 1;
+                nu_cpf2 = nu_cnpj.Substring(0, 13);
+                foreach (var item in nu_cpf2.ToList())
+                {
+
+                   
+                   
+                    switch (posicao)
+                    {
+                        case 1:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores02[0];
+
+                            soma += mult;
+
+                            break;
+                        case 2:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores02[1];
+
+                            soma += mult;
+
+                            break;
+                        case 3:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores02[2];
+
+                            soma += mult;
+
+                            break;
+                        case 4:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores02[3];
+
+                            soma += mult;
+
+                            break;
+                        case 5:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores02[4];
+
+                            soma += mult;
+
+                            break;
+
+                        case 6:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores02[5];
+
+                            soma += mult;
+
+                            break;
+
+                        case 7:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores02[6];
+
+                            soma += mult;
+
+                            break;
+
+                        case 8:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores02[7];
+
+                            soma += mult;
+
+                            break;
+
+                        case 9:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores02[8];
+
+                            soma += mult;
+
+                            break;
+
+                        case 10:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores02[9];
+
+                            soma += mult;
+
+                            break;
+                        case 11:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores02[10];
+
+                            soma += mult;
+
+                            break;
+                        case 12:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores02[11];
+
+                            soma += mult;
+
+                            break;
+                        case 13:
+
+                            mult = int.Parse(item.ToString()) * multiplicadores02[12];
+
+                            soma += mult;
+
+                            break;
+
+                        default:
+                            break;
+
+                    }
+                    posicao +=1;
+                }
+                result = soma % 11;
+                dv2 = int.Parse(nu_cnpj.Substring(13, 1));
+                if (result<2)
+                {
+                    if(result == dv2)
+                    ic_status2 = true;
+                }
+                else if (result >= 2)
+                {
+                    result = 11 - result;
+                    
+
+                    if (result == dv2)
+                    {
+                        ic_status2 = true;
+                    }
+                    
+                }
+
+                if (ic_status1 == true && ic_status2 == true)
+                {
+                    MessageBox.Show("CNPJ é valido.","Informação de validação");
+                }
+                else if (ic_status1 == false && ic_status2 == false)
+                {
+                    MessageBox.Show("CNPJ é inválido.","Informação de validação");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Problema com a validação.");
+
+            }
+
+
+            return icStatus;
+        }
+    }
+}
